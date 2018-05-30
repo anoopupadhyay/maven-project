d1d0d0b6ee21e534f0d8eb68fc6e29297ac94f2b
anoopupadhyay <39748646+anoopupadhyay@users.noreply.github.com>
2018-05-30 15:37:49 -0700

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,6 @@ public class Greeter {
   public Greeter() {
 
   }
-  /**
-  @retrun
-  **/
   
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
